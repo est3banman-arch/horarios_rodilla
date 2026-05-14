4f6a9816a6416d129251575a59a9fd26a0d19e01
est3banman-arch <est3banman@gmail.com>
2026-05-14 09:58:02 +0200

    Create login_rodilla.py

---
+++ login_rodilla.py
@@ -0,0 +1,72 @@
+import streamlit as st
+
+
+
+def mostrar_login():
+    st.markdown("""
+    <style>
+
+    header[data-testid="stHeader"] {
+        opacity: 0;
+        transition: opacity .3s ease;
+    }
+    
+    header[data-testid="stHeader"]:hover {
+        opacity: 1;
+    }
+    .stMainBlockContainer {
+        padding-top: 2rem !important;
+    }
+        
+    [data-testid="stWidgetLabel"] p{
+        font-size: 25px;            
+    }
+                
+    [data-testid="stHeadingWithActionElements"] > h2 {
+        font-size: 40px;            
+    }   
+         
+    div[data-baseweb="select"] > div{
+        height: 60px !important;
+        display: flex;
+        align-items: center;
+        font-size: 25px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    
+    [data-testid="stSelectboxVirtualDropdown"] div {
+        font-size: 25px;
+        display: flex;
+        height: 35px;
+        align-items: center; 
+    }
+    [data-testid="stBaseButton-primary"] div{
+        font-size: 23px;
+    }
+                
+    </style>
+    """,unsafe_allow_html=True)
+
+    st.header("Registro de Horas", divider="gray", text_alignment="center")
+    st.space()
+    # 1. Entrada de usuario
+    nombre = st.selectbox("Selecciona tu nombre", 
+        ["Esteban", "Maria", "Andreina", "Kevin", "Noemi"],
+        index=None,
+        placeholder="Busca tu nombre...")
+    
+    
+    # 2. El boton del login que comprueba lo anterior 
+    
+
+    if st.button("ACCEDER", type="primary", use_container_width=True):
+        if not nombre: 
+            st.error("Selecciona un Usuario")
+        else: 
+            st.session_state.autenticado = True
+            st.session_state.usuario_nombre = nombre
+            st. rerun()
+
+
+
